4851fb175d73a8dcf3a338bf0dea6f5b695228c7
nabil khairy <hammadnabilkhairy@gmail.com>
2024-06-27 19:29:47 +0700

    nih yang baru

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,3 +4,8 @@ mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .styles('resources/css/custom.css', 'public/css/custom.css')
    .sourceMaps();
+   mix.styles([
+      'resources/css/home.css',
+      // tambahkan file CSS lainnya di sini
+  ], 'public/css/all.css');
+  
